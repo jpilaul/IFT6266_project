59b5f49c4672adc71be93095fae6aca21552ade1
jpilault <jonathan.pilault@gmail.com>
2016-04-17 04:31:08 -0400

    using Adam Nesterov learning strategy

--- Models/Model8_Small_Convnet_AdamNesterov.py
+++ Models/Model8_Small_Convnet_AdamNesterov.py
@@ -1,6 +1,5 @@
 """
-Applying Adam Nesterov Momentum
-Convolutional network example.
+Convolutional network example using ReLU
 The original version of this code come from LeNetConvNet.py
 https://github.com/mila-udem/blocks-examples/blob/master/mnist_lenet/
 """
@@ -13,8 +12,8 @@ from theano import tensor
 
 from blocks.algorithms import GradientDescent, Scale, Adam, Momentum
 from Algo_Momentum import NesterovAdam, NesterovMomentum
-from blocks.bricks import (MLP, BatchNormalization, Rectifier,
-Initializable, FeedforwardSequence,Softmax, Activation)
+from blocks.bricks import (MLP, BatchNormalization, SpatialBatchNormalization, BatchNormalizedMLP,
+Rectifier, Initializable, FeedforwardSequence,Softmax, Activation)
 from blocks.bricks.conv import (Convolutional, ConvolutionalSequence, Flattener, MaxPooling)
 from blocks.bricks.cost import CategoricalCrossEntropy, MisclassificationRate
 from blocks.extensions import FinishAfter, Timing, Printing, ProgressBar
@@ -30,13 +29,174 @@ from blocks.model import Model
 from blocks.monitoring import aggregation
 from toolz.itertoolz import interleave
 from fuel_transformers_image import MaximumImageDimensions, RandomHorizontalSwap
-from plot import Plot
 from RRELU_ELU import Rand_Leaky_Rectifier, ELU
 #from ScikitResize import ScikitResize
 import socket
 import datetime
 
 logger = logging.getLogger(__name__)
+
+
+def main(feature_maps=None, mlp_hiddens=None,
+         conv_sizes=None, pool_sizes=None, batch_size=None,
+         num_batches=None):
+    if feature_maps is None:
+        feature_maps = [32, 48, 64, 96, 96, 128]
+    if mlp_hiddens is None:
+        mlp_hiddens = [1000]
+    if conv_sizes is None:
+        conv_sizes = [9, 7, 5, 3, 2, 1]
+    if pool_sizes is None:
+        pool_sizes = [2, 2, 2, 2, 1, 1]
+    if batch_size is None:
+        batch_size = 64
+    conv_steps=[2, 1, 1, 1, 1, 1] #same as stride
+    image_size = (128, 128)
+    output_size = 2
+    learningRate = 0.001
+    drop_prob = 0.4
+    weight_noise = 0.75
+    num_epochs = 150
+    num_batches = None
+    host_plot='http://hades:5090'
+    save_to = "ModelSimpleConvNet128_ELU.pkl"
+    graph_name = 'CNN_convnet_128_ELU'
+    mode = "GPU_run" # "CPU_test" or "GPU_run"
+
+    # Use ReLUs everywhere and softmax for the final prediction
+    conv_activations = [ELU() for _ in feature_maps]
+    mlp_activations = [ELU() for _ in mlp_hiddens] + [Softmax()]
+    convnet = LeNet(conv_activations, 3, image_size,
+                    filter_sizes=zip(conv_sizes, conv_sizes),
+                    feature_maps=feature_maps,
+                    pooling_sizes=zip(pool_sizes, pool_sizes),
+                    conv_steps=zip(conv_steps,conv_steps),
+                    top_mlp_activations=mlp_activations,
+                    top_mlp_dims=mlp_hiddens + [output_size],
+                    border_mode='full',
+                    weights_init=Uniform(width=.2),
+                    biases_init=Constant(0))
+    # We push initialization config to set different initialization schemes
+    # for convolutional layers.
+    convnet.push_initialization_config()
+    convnet.layers[0].weights_init = Uniform(width=.2)
+    convnet.layers[1].weights_init = Constant(0)
+    convnet.top_mlp.linear_transformations[0].weights_init = Uniform(width=.2)
+    convnet.top_mlp.linear_transformations[1].weights_init = Constant(0)
+    convnet.initialize()
+    logging.info("Input dim: {} {} {}".format(
+        *convnet.children[0].get_dim('input_')))
+    for i, layer in enumerate(convnet.layers):
+        if isinstance(layer, Activation):
+            logging.info("Layer {} ({})".format(
+                i, layer.__class__.__name__))
+        else:
+            logging.info("Layer {} ({}) dim: {} {} {}".format(
+                i, layer.__class__.__name__, *layer.get_dim('output')))
+    x = tensor.tensor4('image_features')
+    y = tensor.lmatrix('targets')
+
+    # Normalize input and apply the convnet
+    probs = convnet.apply(x)
+    cost = (CategoricalCrossEntropy().apply(y.flatten(), probs).copy(name='cost'))
+    error_rate = (MisclassificationRate().apply(y.flatten(), probs).copy(name='error_rate'))
+    error_rate2 = error_rate.copy(name='error_rate2')
+
+    cg = ComputationGraph([cost, error_rate])
+    weights = VariableFilter(roles=[FILTER, WEIGHT])(cg.variables)
+
+    ############# Dropout #############
+    """
+    logger.info('Applying dropout')
+    cg = apply_dropout(cg, weights[-1:0], drop_prob) #Dropout only on fully-connected layer
+    dropped_out = VariableFilter(roles=[DROPOUT])(cg.variables)
+
+    ############# Guaussian Noise #############
+
+    logger.info('Applying Gaussian noise')
+    cg = apply_noise(cg, weights, weight_noise)
+    """
+    ########### Loading images #####################
+
+    from fuel.datasets.dogs_vs_cats import DogsVsCats
+    from fuel.streams import DataStream, ServerDataStream
+    from fuel.schemes import ShuffledScheme
+    from fuel.transformers.image import RandomFixedSizeCrop, MinimumImageDimensions, Random2DRotation
+    from fuel.transformers import Flatten, Cast, ScaleAndShift
+
+    def create_data(data):
+
+        stream = DataStream(data, iteration_scheme=ShuffledScheme(data.num_examples, batch_size))
+
+        # Data Augmentation
+        stream = MinimumImageDimensions(stream, image_size, which_sources=('image_features',))
+        stream = MaximumImageDimensions(stream, image_size, which_sources=('image_features',))
+        stream = RandomHorizontalSwap(stream, which_sources=('image_features',))
+        stream = Random2DRotation(stream, which_sources=('image_features',))
+        #stream = ScikitResize(stream, image_size, which_sources=('image_features',))
+
+        # Data Preprocessing
+
+        # Data Transformation
+        stream = ScaleAndShift(stream, 1./255, 0, which_sources=('image_features',))
+        stream = Cast(stream, dtype='float32', which_sources=('image_features',))
+        return stream
+
+
+    if mode is "CPU_test":
+        stream_data_train = create_data(DogsVsCats(('train',), subset=slice(0, 100)))
+        stream_data_valid = create_data(DogsVsCats(('train',), subset=slice(100, 110)))
+    if mode is  "GPU_run":
+        stream_data_train = create_data(DogsVsCats(('train',), subset=slice(0, 22500)))
+        stream_data_valid = create_data(DogsVsCats(('train',), subset=slice(22500, 25000)))
+    if mode is "data_server":
+        stream_data_train = ServerDataStream(('image_features','targets'), False, port=5560)
+        stream_data_valid = ServerDataStream(('image_features','targets'), False, port=5561)
+
+
+    # Train with simple SGD
+    # On the importance of initialization and momentum in deep learning: choose lowest momentum w/ lowest error
+    algorithm = GradientDescent(cost=cost, parameters=cg.parameters,step_rule=NesterovAdam(learning_rate=learningRate, momentum=0.995))
+    #algorithm = GradientDescent(cost=cost, parameters=cg.parameters,step_rule=Scale(learning_rate=learningRate))
+    #algorithm = GradientDescent(cost=cost, parameters=cg.parameters,step_rule=Adam(0.001))
+
+
+    # `Timing` extension reports time for reading data, aggregating a batch
+    # and monitoring;
+    # `ProgressBar` displays a nice progress bar during training.
+    extensions = []
+    extensions.append(Timing())
+    extensions.append(FinishAfter(after_n_epochs=num_epochs,after_n_batches=num_batches))
+    extensions.append(DataStreamMonitoring([cost, error_rate],stream_data_valid,prefix="valid"))
+    extensions.append(TrainingDataMonitoring([cost, error_rate,aggregation.mean(algorithm.total_gradient_norm)],prefix="train",after_epoch=True))
+    extensions.append(ProgressBar())
+    extensions.append(Printing())
+
+    if mode == ("GPU_run" or "data_server"):
+        try:
+            from plot import Plot
+            extensions.append(Plot('%s %s @ %s' % (graph_name, datetime.datetime.now(), socket.gethostname()),
+                                channels=[['train_error_rate', 'valid_error_rate'],
+                                 ['train_total_gradient_norm']], after_epoch=True, server_url=host_plot))
+            PLOT_AVAILABLE = True
+        except ImportError:
+            PLOT_AVAILABLE = False
+        extensions.append(Checkpoint(save_to, after_epoch=True, after_training=True, save_separately=['log']))
+
+
+    logger.info("Building the model")
+
+    model = Model(cost)
+
+    ########### Loading images #####################
+    main_loop = MainLoop(
+        algorithm,
+        stream_data_train,
+        model=model,
+        extensions=extensions)
+
+    main_loop.run()
+
 
 class LeNet(FeedforwardSequence, Initializable):
     """LeNet-like convolutional network.
@@ -76,36 +236,33 @@ class LeNet(FeedforwardSequence, Initializable):
     """
 
     def __init__(self, conv_activations, num_channels, image_shape,
-                 filter_sizes, feature_maps, pooling_sizes,
-                 top_mlp_activations, top_mlp_dims,
-                 conv_step=None, border_mode='valid', **kwargs):
-        if conv_step is None:
-            self.conv_step = (1, 1)
-        else:
-            self.conv_step = conv_step
+                 filter_sizes, feature_maps, conv_steps, pooling_sizes,
+                 top_mlp_activations, top_mlp_dims, border_mode='valid', **kwargs):
+
         self.num_channels = num_channels
         self.image_shape = image_shape
         self.top_mlp_activations = top_mlp_activations
         self.top_mlp_dims = top_mlp_dims
         self.border_mode = border_mode
 
-        conv_parameters = zip(filter_sizes, feature_maps)
+        conv_parameters = zip(filter_sizes, feature_maps, conv_steps)
 
         # Construct convolutional, activation, and pooling layers with corresponding parameters
-        self.layers = list(interleave([
+        conv_layers = list(interleave([
             (Convolutional(filter_size=filter_size,
                            num_filters=num_filter,
-                           step=self.conv_step,
+                           step=conv_step,
                            border_mode=self.border_mode,
                            name='conv_{}'.format(i))
-             for i, (filter_size, num_filter)
+             for i, (filter_size, num_filter, conv_step)
              in enumerate(conv_parameters)),
-             (BatchNormalization(name='bn_conv_{}'.format(i))
-             for i in enumerate(conv_parameters)),
              conv_activations,
              (MaxPooling(size, name='pool_{}'.format(i))
              for i, size in enumerate(pooling_sizes))]))
 
+        # Applying SpatialBatchNormalization to inputs
+        #self.layers = [SpatialBatchNormalization()] + conv_layers
+        self.layers = conv_layers
         self.conv_sequence = ConvolutionalSequence(self.layers, num_channels,
                                                    image_size=image_shape)
 
@@ -136,164 +293,22 @@ class LeNet(FeedforwardSequence, Initializable):
         self.top_mlp.dims = [numpy.prod(conv_out_dim)] + self.top_mlp_dims
 
 
-def main(save_to, num_epochs, feature_maps=None, mlp_hiddens=None,
-         conv_sizes=None, pool_sizes=None, batch_size=200,
-         num_batches=None):
-    if feature_maps is None:
-        feature_maps = [32, 64, 64, 128, 128]
-    if mlp_hiddens is None:
-        mlp_hiddens = [1000]
-    if conv_sizes is None:
-        conv_sizes = [7, 5, 5, 3, 3]
-    if pool_sizes is None:
-        pool_sizes = [2, 2, 2, 2, 2]
-    image_size = (84, 84)
-    batch_size = 64
-    output_size = 2
-    learningRate = 0.01
-    drop_prob = 0.4
-    weight_noise = 0.75
-    num_epochs = 150
-    num_batches = None
-
-
-
-    # Use ReLUs everywhere and softmax for the final prediction
-    conv_activations = [Rectifier() for _ in feature_maps]
-    mlp_activations = [Rectifier() for _ in mlp_hiddens] + [Softmax()]
-    convnet = LeNet(conv_activations, 3, image_size,
-                    filter_sizes=zip(conv_sizes, conv_sizes),
-                    feature_maps=feature_maps,
-                    pooling_sizes=zip(pool_sizes, pool_sizes),
-                    top_mlp_activations=mlp_activations,
-                    top_mlp_dims=mlp_hiddens + [output_size],
-                    border_mode='full',
-                    weights_init=IsotropicGaussian(std = 0.2, mean = 0),
-                    biases_init=IsotropicGaussian(std = 0.09, mean = 0))
-    # We push initialization config to set different initialization schemes
-    # for convolutional layers.
-    convnet.push_initialization_config()
-    convnet.layers[0].weights_init = IsotropicGaussian(std = 0.2, mean = 0)
-    convnet.layers[1].weights_init = IsotropicGaussian(std = 0.09, mean = 0)
-    convnet.top_mlp.linear_transformations[0].weights_init = IsotropicGaussian(std = 0.8, mean = 0)
-    convnet.top_mlp.linear_transformations[1].weights_init = IsotropicGaussian(std = 0.11, mean = 0)
-    convnet.initialize()
-    logging.info("Input dim: {} {} {}".format(
-        *convnet.children[0].get_dim('input_')))
-    for i, layer in enumerate(convnet.layers):
-        if isinstance(layer, Activation):
-            logging.info("Layer {} ({})".format(
-                i, layer.__class__.__name__))
-        else:
-            logging.info("Layer {} ({}) dim: {} {} {}".format(
-                i, layer.__class__.__name__, *layer.get_dim('output')))
-    x = tensor.tensor4('image_features')
-    y = tensor.lmatrix('targets')
-
-    # Normalize input and apply the convnet
-    probs = convnet.apply(x)
-    cost = (CategoricalCrossEntropy().apply(y.flatten(), probs).copy(name='cost'))
-    error_rate = (MisclassificationRate().apply(y.flatten(), probs).copy(name='error_rate'))
-    error_rate2 = error_rate.copy(name='error_rate2')
-
-    cg = ComputationGraph([cost, error_rate])
-    weights = VariableFilter(roles=[FILTER, WEIGHT])(cg.variables)
-
-    ############# Dropout #############
-
-    logger.info('Applying dropout')
-    cg = apply_dropout(cg, weights[-1:0], drop_prob) #Dropout only on fully-connected layer
-    dropped_out = VariableFilter(roles=[DROPOUT])(cg.variables)
-
-    ############# Guaussian Noise #############
-    """
-    logger.info('Applying Gaussian noise')
-    cg = apply_noise(cg, weights, weight_noise)
-    """
-    ########### Loading images #####################
-
-    from fuel.datasets.dogs_vs_cats import DogsVsCats
-    from fuel.streams import DataStream, ServerDataStream
-    from fuel.schemes import ShuffledScheme
-    from fuel.transformers.image import RandomFixedSizeCrop, MinimumImageDimensions, Random2DRotation
-    from fuel.transformers import Flatten, Cast, ScaleAndShift
-
-    def create_data(data):
-        stream = DataStream(data, iteration_scheme=ShuffledScheme(data.num_examples, batch_size))
-        stream = MinimumImageDimensions(stream, image_size, which_sources=('image_features',))
-        stream = MaximumImageDimensions(stream, image_size, which_sources=('image_features',))
-        stream = RandomHorizontalSwap(stream, which_sources=('image_features',))
-        stream = Random2DRotation(stream, which_sources=('image_features',))
-        #stream = ScikitResize(stream, image_size, which_sources=('image_features',))
-        stream = ScaleAndShift(stream, 1./255, 0, which_sources=('image_features',))
-        stream = Cast(stream, dtype='float32', which_sources=('image_features',))
-        return stream
-
-    #stream_data_train = ServerDataStream(('image_features','targets'), False, port=5560)
-    #stream_data_valid = ServerDataStream(('image_features','targets'), False, port=5561)
-    stream_data_train = create_data(DogsVsCats(('train',), subset=slice(0, 22500)))
-    stream_data_valid = create_data(DogsVsCats(('train',), subset=slice(22500, 25000)))
-    #stream_data_train = create_data(DogsVsCats(('train',), subset=slice(0, 10)))
-    #stream_data_valid = create_data(DogsVsCats(('train',), subset=slice(10, 12)))
-
-    # Train with simple SGD
-    # On the importance of initialization and momentum in deep learning: choose lowest momentum w/ lowest error
-    algorithm = GradientDescent(cost=cost, parameters=cg.parameters,step_rule=NesterovAdam(learning_rate=learningRate, momentum=0.995))
-    #algorithm = GradientDescent(cost=cost, parameters=cg.parameters,step_rule=Scale(learning_rate=learningRate))
-    #algorithm = GradientDescent(cost=cost, parameters=cg.parameters,step_rule=Adam(0.001))
-
-
-    # `Timing` extension reports time for reading data, aggregating a batch
-    # and monitoring;
-    # `ProgressBar` displays a nice progress bar during training.
-    extensions = []
-    extensions.append(Timing())
-    extensions.append(FinishAfter(after_n_epochs=num_epochs,after_n_batches=num_batches))
-    extensions.append(DataStreamMonitoring([cost, error_rate],stream_data_valid,prefix="valid"))
-    extensions.append(TrainingDataMonitoring([cost, error_rate,aggregation.mean(algorithm.total_gradient_norm)],prefix="train",after_epoch=True))
-    extensions.append(Checkpoint("Model8_AdamNesterov.pkl", after_epoch=True, after_training=True, save_separately=['log']))
-    extensions.append(ProgressBar())
-    extensions.append(Printing())
-
-    host_plot='http://hades:5090'
-    extensions.append(Plot('%s %s @ %s' % ('CNN8_AdamNesterov', datetime.datetime.now(), socket.gethostname()),
-                        channels=[['train_error_rate', 'valid_error_rate'],
-                         ['train_total_gradient_norm']], after_epoch=True, server_url=host_plot))
-    logger.info("Building the model")
-
-    model = Model(cost)
-
-    ########### Loading images #####################
-    main_loop = MainLoop(
-        algorithm,
-        stream_data_train,
-        model=model,
-        extensions=extensions)
-
-    main_loop.run()
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     parser = ArgumentParser("An example of training a convolutional network "
                             "on the CatVsDog dataset.")
-    parser.add_argument("--num-epochs", type=int, default=2,
-                        help="Number of training epochs to do.")
-    parser.add_argument("save_to", default="mnist.pkl", nargs="?",
-                        help="Destination to save the state of the training "
-                             "process.")
     parser.add_argument("--feature-maps", type=int, nargs='+',
-                        default=[32, 32, 64, 64, 128, 128], help="List of feature maps numbers.")
-    parser.add_argument("--mlp-hiddens", type=int, nargs='+', default=[20],
+                        default=None, help="List of feature maps numbers.")
+    parser.add_argument("--mlp-hiddens", type=int, nargs='+', default=None,
                         help="List of numbers of hidden units for the MLP.")
-    parser.add_argument("--conv-sizes", type=int, nargs='+', default=[7, 5, 5, 5, 3, 3],
+    parser.add_argument("--conv-sizes", type=int, nargs='+', default=None,
                         help="Convolutional kernels sizes. The kernels are "
                         "always square.")
-    parser.add_argument("--pool-sizes", type=int, nargs='+', default=[2, 2, 2, 2, 2, 2],
+    parser.add_argument("--pool-sizes", type=int, nargs='+', default=None,
                         help="Pooling sizes. The pooling windows are always "
                              "square. Should be the same length as "
                              "--conv-sizes.")
-    parser.add_argument("--batch-size", type=int, default=200,
+    parser.add_argument("--batch-size", type=int, default=None,
                         help="Batch size.")
     args = parser.parse_args()
     main(**vars(args))
